466948ea5988668fc57bda9e1e71597d326e795f
Boppy <no-reply@boppygames.gg>
2022-10-30 11:00:11 +0000

    Fixed tap

--- Formula/spacetime.rb
+++ Formula/spacetime.rb
@@ -5,7 +5,7 @@ class Spacetime < Formula
 
   version "0.2.0"
   url "http://install.spacetimedb.net/0.2.0/spacetime-0.2.0.darwin-universal.tgz"
-  sha256 "10e4634940f76ad2460ae86e5ae72da82a61c0f08710180aa0d55f363cd3d64a"
+  sha256 "86e4e83e8a959fcf88058f4e3848744d40c2388cebc3ca75bee0c6c49ca8cc42"
 
   def install
     bin.install "spacetime"
