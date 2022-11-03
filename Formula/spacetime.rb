c4041a5f522922cbcc2e59b302a6601052f171a9
Boppy <no-reply@boppygames.gg>
2022-11-03 11:48:37 +0000

    Version 0.2.4

--- Formula/spacetime.rb
+++ Formula/spacetime.rb
@@ -3,9 +3,9 @@ class Spacetime < Formula
   desc "A command line interface for SpacetimeDB"
   homepage "https://spacetimedb.net"
 
-  version "0.2.3"
-  url "http://install.spacetimedb.net/0.2.3/spacetime-0.2.3.darwin-universal.tgz"
-  sha256 "57a6256e5a0f536bdd6d9f14543b8e11a30b4cc7939bcc0d3eb0707ccd01fbdd"
+  version "0.2.4"
+  url "http://install.spacetimedb.net/0.2.4/spacetime-0.2.4.darwin-universal.tgz"
+  sha256 "10e8ea86279b3aab4672af079a2ca2c2554be4018445619b98c9ebf6e3e420ce"
 
   def install
     bin.install "spacetime"
