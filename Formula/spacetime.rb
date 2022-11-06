fc4350fb81d1492fc6db2864529f451b45e3be2f
Clockwork Labs <no-reply@clockworklabs.io>
2022-11-06 22:03:13 +0000

    <AUTO> Committing upgrade to version 0.2.4

--- Formula/spacetime.rb
+++ Formula/spacetime.rb
@@ -5,7 +5,7 @@ class Spacetime < Formula
 
   version "0.2.4"
   url "http://install.spacetimedb.net/0.2.4/spacetime-0.2.4.darwin-universal.tgz"
-  sha256 "10e8ea86279b3aab4672af079a2ca2c2554be4018445619b98c9ebf6e3e420ce"
+  sha256 "c95e104fe8e6dec4d4e55f6a7198a869e7ee9688b8fc97aaeca8a9775881389e"
 
   def install
     bin.install "spacetime"
