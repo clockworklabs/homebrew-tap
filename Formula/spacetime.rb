2c4e8b5dabccfda58cdbcf3a40635a7a6b836535
John <no-reply@boppygames.gg>
2023-02-24 19:36:26 -0600

    Brew tap upgraded to 1.0.0

--- Formula/spacetime.rb
+++ Formula/spacetime.rb
@@ -3,7 +3,7 @@ class Spacetime < Formula
   desc "A command line interface for SpacetimeDB"
   homepage "https://spacetimedb.com"
 
-  version "0.3.2"
+  version "1.0.0"
   url "http://install.spacetimedb.net/spacetime-homebrew.tar.gz"
   sha256 "f9771bf103d93474bb3bbb702045abeb6670c64d71124c05a5789098619d1c6d"
 
