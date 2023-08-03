22d432ee0cded950acae798f1044bb52ffe80841
Boppy <no-reply@boppygames.gg>
2023-08-03 07:30:12 -0500

    Updated brew release to 0.6.0

--- Formula/spacetime.rb
+++ Formula/spacetime.rb
@@ -3,9 +3,16 @@ class Spacetime < Formula
   desc "A command line interface for SpacetimeDB"
   homepage "https://spacetimedb.com"
 
-  version "1.0.0"
-  url "https://install.spacetimedb.com/spacetime-homebrew.tar.gz"
-  sha256 "f9771bf103d93474bb3bbb702045abeb6670c64d71124c05a5789098619d1c6d"
+  version "0.6.0"
+
+  if Hardware::CPU.arm?
+    url "https://github.com/clockworklabs/SpacetimeDB/releases/latest/download/spacetime.darwin-arm64"
+    sha256 "68e44886761721618714e283f2b18951d512d9899aec99477be50303b787676d"
+  else
+    url "https://github.com/clockworklabs/SpacetimeDB/releases/latest/download/spacetime.darwin-amd64"
+    sha256 "1dfabb56464e0aadf85c2b2cb7a17f2c51f8878f322866f70bd7a7b92446c927"
+  end
+
 
   def install
     bin.install "spacetime"
