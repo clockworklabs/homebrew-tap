94719ec024f51915097f2a1425d4192b66095df4
Boppy <no-reply@boppygames.gg>
2023-08-17 16:37:53 -0500

    Updated hash for AMD install

--- Formula/spacetime.rb
+++ Formula/spacetime.rb
@@ -10,7 +10,7 @@ class Spacetime < Formula
     sha256 "40c553be7246d709a08ccf07a20e73db2503127853c256b3de9fff4a95062db2"
   else
     url "https://github.com/clockworklabs/SpacetimeDB/releases/latest/download/spacetime.darwin-amd64.tar.gz"
-    sha256 "b92d0303fae98ad582adb4e45a3eb6689f440de03c3fea97e461fba659a6edf4"
+    sha256 "f6a1a3cc8254ca41a50f08bc99ed131b9d07a889b3293fa57f2e48b8987201e9"
   end
 
 
