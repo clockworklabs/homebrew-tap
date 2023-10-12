1c6b530f5f2261b10bcd8c2404ac7ab21f510ab2
John Detter <no-reply@boppygames.gg>
2023-10-11 23:31:08 -0500

    Updated the formula for 0.7.0, removed outdated release file

--- Formula/spacetime.rb
+++ Formula/spacetime.rb
@@ -3,14 +3,14 @@ class Spacetime < Formula
   desc "A command line interface for SpacetimeDB"
   homepage "https://spacetimedb.com"
 
-  version "0.6.1"
+  version "0.7.0"
 
   if Hardware::CPU.arm?
-    url "https://github.com/clockworklabs/SpacetimeDB/releases/latest/download/spacetime.darwin-arm64.tar.gz"
-    sha256 "40c553be7246d709a08ccf07a20e73db2503127853c256b3de9fff4a95062db2"
+    url "https://github.com/clockworklabs/SpacetimeDB/releases/download/v0.7.0-beta/spacetime.darwin-arm64.tar.gz"
+    sha256 "2e4d37bb4cfc8a7b5d2e1046a6cebde9718a7cd784138f4710b5e285dd8c11d8"
   else
-    url "https://github.com/clockworklabs/SpacetimeDB/releases/latest/download/spacetime.darwin-amd64.tar.gz"
-    sha256 "f6a1a3cc8254ca41a50f08bc99ed131b9d07a889b3293fa57f2e48b8987201e9"
+    url "https://github.com/clockworklabs/SpacetimeDB/releases/download/v0.7.0-beta/spacetime.darwin-amd64.tar.gz"
+    sha256 "8d6f4346ed6d2aeca750952b7437174b92c55d1d06ca00b2c36d11e6586515ec"
   end
 
 
