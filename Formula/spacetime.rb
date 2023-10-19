9750fee61e26ee56699003a13b5de8ddb3f778ad
John Detter <no-reply@boppygames.gg>
2023-10-19 12:06:43 -0500

    Use hotfix instead

--- Formula/spacetime.rb
+++ Formula/spacetime.rb
@@ -6,10 +6,10 @@ class Spacetime < Formula
   version "0.7.1"
 
   if Hardware::CPU.arm?
-    url "https://github.com/clockworklabs/SpacetimeDB/releases/download/v0.7.1-beta/spacetime.darwin-arm64.tar.gz"
+    url "https://github.com/clockworklabs/SpacetimeDB/releases/download/v0.7.1-beta-hotfix1/spacetime.darwin-arm64.tar.gz"
     sha256 "df67f355be45ec256e4a627a0fbc9df1487c3df8d3fa49d3ca0ad1d6889ec7bf"
   else
-    url "https://github.com/clockworklabs/SpacetimeDB/releases/download/v0.7.1-beta/spacetime.darwin-amd64.tar.gz"
+    url "https://github.com/clockworklabs/SpacetimeDB/releases/download/v0.7.1-beta-hotfix1/spacetime.darwin-amd64.tar.gz"
     sha256 "92a726a36a371a95d8a008c220b3cec92f8f8ab14b5b599bcb4fa9f912c73947"
   end
 
