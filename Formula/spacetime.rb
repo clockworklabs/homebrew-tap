1dbcb16f9dcb13f91e1a292f2ce20af8c3a165e2
John Detter <4099508+jdetter@users.noreply.github.com>
2024-01-19 10:03:09 -0600

    Release v0.8.1-beta (#8)
    Co-authored-by: John Detter <no-reply@boppygames.gg>

--- Formula/spacetime.rb
+++ Formula/spacetime.rb
@@ -3,14 +3,14 @@ class Spacetime < Formula
   desc "A command line interface for SpacetimeDB"
   homepage "https://spacetimedb.com"
 
-  version "0.8.0"
+  version "0.8.1"
 
   if Hardware::CPU.arm?
-    url "https://github.com/clockworklabs/SpacetimeDB/releases/download/v0.8.0-beta/spacetime.darwin-arm64.tar.gz"
-    sha256 "b0e794fd88682b37fee80cbbf86eb586155dcb2ab221aeac4c6dc7b993e5cfc1"
+    url "https://github.com/clockworklabs/SpacetimeDB/releases/download/v0.8.1-beta/spacetime.darwin-arm64.tar.gz"
+    sha256 "0fd3f615e15a8ebf8293cad5d936cf82b8d84809cc61b9be044ecdf2605e57ec"
   else
-    url "https://github.com/clockworklabs/SpacetimeDB/releases/download/v0.8.0-beta/spacetime.darwin-amd64.tar.gz"
-    sha256 "e3039e34922ca53aad2feeb761863499b57e5055e7e3e6466d537d8fa886986c"
+    url "https://github.com/clockworklabs/SpacetimeDB/releases/download/v0.8.1-beta/spacetime.darwin-amd64.tar.gz"
+    sha256 "87e185c91040b6ee942049637ddb2a86f64b9374647f685cbb2f0d661845777f"
   end
 
 
