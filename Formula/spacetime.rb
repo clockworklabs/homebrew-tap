6a8251f8799b3eae72c914c3494d3be4f2a51acf
John Detter <4099508+jdetter@users.noreply.github.com>
2024-05-04 14:28:12 -0500

    Updated Intel build for 0.9.0

--- Formula/spacetime.rb
+++ Formula/spacetime.rb
@@ -10,7 +10,7 @@ class Spacetime < Formula
     sha256 "2a2069285b91b0589c6d281468232b7c6028dcf73eabcf905f9b6d83fee17e41"
   else
     url "https://github.com/clockworklabs/SpacetimeDB/releases/download/v0.9.0-beta/spacetime.darwin-amd64.tar.gz"
-    sha256 "a51513b5f0f967755881c87de8e085a6ae059760cf04981bcb089961d2ee0cd5"
+    sha256 "86d48bd56c1d9c8a8c826a5ffd6fa9a57408b7c258eb417e2cb6e50befe3b9d7"
   end
 
 
