25a3bf7eb689279287220ea8d489778d7c26c72c
Kurtis Mullins <github@kurtismullins.com>
2024-06-13 14:56:27 -0400

    v0.10.0-beta

--- Formula/spacetime.rb
+++ Formula/spacetime.rb
@@ -6,11 +6,11 @@ class Spacetime < Formula
   version "0.8.2"
 
   if Hardware::CPU.arm?
-    url "https://github.com/clockworklabs/SpacetimeDB/releases/download/v0.9.2-beta/spacetime.darwin-arm64.tar.gz"
-    sha256 "d4e4d58181d4de65652f4a48adc4220d97ea27b5be15eef21fb7423069c91917"
+    url "https://github.com/clockworklabs/SpacetimeDB/releases/download/v0.10.0-beta/spacetime.darwin-arm64.tar.gz"
+    sha256 "572edb01f4617af4b63b5ae54ea08e3b6da21fccd4980588d7324e1cfbc835e3"
   else
-    url "https://github.com/clockworklabs/SpacetimeDB/releases/download/v0.9.2-beta/spacetime.darwin-amd64.tar.gz"
-    sha256 "2251be77451ec845241cd95f35e9b86e0b2805673127d0468c86fc027842b610"
+    url "https://github.com/clockworklabs/SpacetimeDB/releases/download/v0.10.0-beta/spacetime.darwin-amd64.tar.gz"
+    sha256 "827cc7695a16debd955db6dcb5767babb3eda4a57691c21b7f0e2d378ddf6b05"
   end
 
 
