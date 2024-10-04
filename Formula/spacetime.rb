291cdb1d96f590eb5c0e1e25c9ef06cc776ac76f
John Detter <4099508+jdetter@users.noreply.github.com>
2024-10-04 00:19:15 -0500

    Release 0.12.0-beta (#18)
    Co-authored-by: John Detter <no-reply@boppygames.gg>

--- Formula/spacetime.rb
+++ Formula/spacetime.rb
@@ -3,14 +3,14 @@ class Spacetime < Formula
   desc "A command line interface for SpacetimeDB"
   homepage "https://spacetimedb.com"
 
-  version "0.10.1"
+  version "0.12.0"
 
   if Hardware::CPU.arm?
-    url "https://github.com/clockworklabs/SpacetimeDB/releases/download/v0.11.0-beta/spacetime.darwin-arm64.tar.gz"
-    sha256 "e626102545433d1f7ef589706308294a56f1c35abb08e29c813a31d1a4e7230b"
+    url "https://github.com/clockworklabs/SpacetimeDB/releases/download/v0.12.0-beta/spacetime.darwin-arm64.tar.gz"
+    sha256 "d7949c92c9ba9ccfe2254a3a95ceed07655ec24d852b32e1ca4b3e94e08fa29d"
   else
-    url "https://github.com/clockworklabs/SpacetimeDB/releases/download/v0.11.0-beta/spacetime.darwin-amd64.tar.gz"
-    sha256 "5a494a5c8a24dad18aa8dc69d8b607b3af08b1f2dbc9456d73c19efd80ab994d"
+    url "https://github.com/clockworklabs/SpacetimeDB/releases/download/v0.12.0-beta/spacetime.darwin-amd64.tar.gz"
+    sha256 "33395666ce1cda47537558e02572fbb46dc8bbfbd2d2f7aef6c1d365a6c63ca0"
   end
 
 
@@ -22,4 +22,3 @@ class Spacetime < Formula
     system "#{bin}/spacetime version"
   end
 end
-
