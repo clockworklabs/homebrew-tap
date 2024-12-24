73ef0518103dd3dadbda9ac4bac46fe9b181257d
Zeke Foppa <bfops@users.noreply.github.com>
2024-12-23 22:38:05 -0800

    update tap

--- Formula/spacetime.rb
+++ Formula/spacetime.rb
@@ -6,11 +6,11 @@ class Spacetime < Formula
   version "1.0.0-rc2"
 
   if Hardware::CPU.arm?
-    url "https://github.com/clockworklabs/SpacetimeDB/releases/download/v1.0.0-rc2-hotfix1/spacetime.darwin-arm64.tar.gz"
-    sha256 "cb3c99435177a897b4a987528ce276440ae94781534d88555401bc45f0060a3b"
+    url "https://github.com/clockworklabs/SpacetimeDB/releases/download/v1.0.0-rc2-hotfix2/spacetime.darwin-arm64.tar.gz"
+    sha256 "4cddd78a92a98cdd4f2870779f271f8204424d8b788f76248d4dc626d5e5b86a"
   else
-    url "https://github.com/clockworklabs/SpacetimeDB/releases/download/v1.0.0-rc2-hotfix1/spacetime.darwin-amd64.tar.gz"
-    sha256 "f69c6fce4a75f32b2374ff15b32f35b33773bc910c1ec0186dede0a40b731ee1"
+    url "https://github.com/clockworklabs/SpacetimeDB/releases/download/v1.0.0-rc2-hotfix2/spacetime.darwin-amd64.tar.gz"
+    sha256 "56bf7f3d381663dc674b2b45d0fc3309c63e7f038b97f1f36c09430a28cc8c9b"
   end
 
 
