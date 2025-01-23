4552388b866f9037bffca4657d000cbbebeb3923
Zeke Foppa <196249+bfops@users.noreply.github.com>
2025-01-23 13:34:22 -0800

    Release v1.0.0-rc3-hotfix2 (#27)
    * Update to hotfix2
    
    * empty commit to bump CI
    
    ---------
    
    Co-authored-by: Zeke Foppa <bfops@users.noreply.github.com>

--- Formula/spacetime.rb
+++ Formula/spacetime.rb
@@ -6,11 +6,11 @@ class Spacetime < Formula
   version "1.0.0-rc3"
 
   if Hardware::CPU.arm?
-    url "https://github.com/clockworklabs/SpacetimeDB/releases/download/v1.0.0-rc3-hotfix1/spacetime.darwin-arm64.tar.gz"
-    sha256 "230740fee768fcc62aa0882d705b107a8715ebcbb7c3c125310919c8e7e2169f"
+    url "https://github.com/clockworklabs/SpacetimeDB/releases/download/v1.0.0-rc3-hotfix2/spacetime.darwin-arm64.tar.gz"
+    sha256 "38f54aff4ef5f5bb80efa57fea85eeedb5ecf69f78750f041b6193d994daca46"
   else
-    url "https://github.com/clockworklabs/SpacetimeDB/releases/download/v1.0.0-rc3-hotfix1/spacetime.darwin-amd64.tar.gz"
-    sha256 "3a64e7e72620a9af4d50db95d2d37f6c00bb64e0c55332b610e613c6d39b44bc"
+    url "https://github.com/clockworklabs/SpacetimeDB/releases/download/v1.0.0-rc3-hotfix2/spacetime.darwin-amd64.tar.gz"
+    sha256 "0ab4d7e18cf6a365f5af1d82b247f54af393de9c89f29825a5a6668fe4bddc2b"
   end
 
 
