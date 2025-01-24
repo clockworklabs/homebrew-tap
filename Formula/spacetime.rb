da06da6774d08585910b8aba575aae6a4ec3db2f
Zeke Foppa <196249+bfops@users.noreply.github.com>
2025-01-24 15:07:39 -0800

    update to v1.0.0-rc3-hotfix4 (#28)
    Co-authored-by: Zeke Foppa <bfops@users.noreply.github.com>

--- Formula/spacetime.rb
+++ Formula/spacetime.rb
@@ -6,11 +6,11 @@ class Spacetime < Formula
   version "1.0.0-rc3"
 
   if Hardware::CPU.arm?
-    url "https://github.com/clockworklabs/SpacetimeDB/releases/download/v1.0.0-rc3-hotfix2/spacetime.darwin-arm64.tar.gz"
-    sha256 "38f54aff4ef5f5bb80efa57fea85eeedb5ecf69f78750f041b6193d994daca46"
+    url "https://github.com/clockworklabs/SpacetimeDB/releases/download/v1.0.0-rc3-hotfix4/spacetime.darwin-arm64.tar.gz"
+    sha256 "dc6f632e80fdd812bf6cbeb332a520fd4336c9afdf25e08c526c4dc71a6be987"
   else
-    url "https://github.com/clockworklabs/SpacetimeDB/releases/download/v1.0.0-rc3-hotfix2/spacetime.darwin-amd64.tar.gz"
-    sha256 "0ab4d7e18cf6a365f5af1d82b247f54af393de9c89f29825a5a6668fe4bddc2b"
+    url "https://github.com/clockworklabs/SpacetimeDB/releases/download/v1.0.0-rc3-hotfix4/spacetime.darwin-amd64.tar.gz"
+    sha256 "3fea894e2d265318e07eda609626d571a559cee943e2eda89e0cc4775c95881a"
   end
 
 
