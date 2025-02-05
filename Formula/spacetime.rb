9f779802a2263ac826895cc0b734551ea5e6bc9c
Zeke Foppa <bfops@users.noreply.github.com>
2025-02-05 14:19:37 -0800

    fix sha?

--- Formula/spacetime.rb
+++ Formula/spacetime.rb
@@ -7,10 +7,10 @@ class Spacetime < Formula
 
   if Hardware::CPU.arm?
     url "https://github.com/clockworklabs/SpacetimeDB/releases/download/v1.0.0-rc3-hotfix7/spacetime.darwin-arm64.tar.gz"
-    sha256 "23adc671e53418a5cd73e3962832e75cb60647abd3d56230c12f594e78319444"
+    sha256 "ba07bd8e15c8ea7b8c64e863c32e1ba93aaca0ea48486047b4e749d4b49e96c6"
   else
     url "https://github.com/clockworklabs/SpacetimeDB/releases/download/v1.0.0-rc3-hotfix7/spacetime.darwin-amd64.tar.gz"
-    sha256 "bdc77fb264bb6b7bc997d2ca13cf85d2f1cd11bbee93305d84f9d177c3278cfa"
+    sha256 "540ee00b67f34127bea709818cd2a6374dfbcba40fd551e4c45175a54ba3bc64"
   end
 
 
