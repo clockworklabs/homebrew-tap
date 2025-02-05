a7adc59cbe1342434585d072cdef9be53eb35a31
Zeke Foppa <bfops@users.noreply.github.com>
2025-02-05 13:57:24 -0800

    fix typo

--- Formula/spacetime.rb
+++ Formula/spacetime.rb
@@ -9,7 +9,7 @@ class Spacetime < Formula
     url "https://github.com/clockworklabs/SpacetimeDB/releases/download/v1.0.0-rc3-hotfix7/spacetime.darwin-arm64.tar.gz"
     sha256 "23adc671e53418a5cd73e3962832e75cb60647abd3d56230c12f594e78319444"
   else
-    url "https://github.com/clockworklabs/SpacetimeDB/releases/download/v1.0.0-rc3-hotfix7/spacetime.darwin-arm64.tar.gz"
+    url "https://github.com/clockworklabs/SpacetimeDB/releases/download/v1.0.0-rc3-hotfix7/spacetime.darwin-amd64.tar.gz"
     sha256 "bdc77fb264bb6b7bc997d2ca13cf85d2f1cd11bbee93305d84f9d177c3278cfa"
   end
 
